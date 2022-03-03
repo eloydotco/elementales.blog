455b65a1ac658398beb7c89ee88d09be906ab6a6
Eloy Castelo <e.castelo.otero@gmail.com>
2022-03-03 18:59:06 +0100

    fix: make automatic deployment work v2

--- setup/deploy.js
+++ setup/deploy.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const GhostAdminApi = require('@tryghost/admin-api');
 
+const path = require('path');
+const fs = require('fs');
+//joining path of directory
+const directoryPath = path.join(process.cwd(), 'liebling-custom.zip');
+//passsing directoryPath and callback function
+
 (async function main() {
   try {
     const api = new GhostAdminApi({
@@ -9,8 +15,27 @@ const GhostAdminApi = require('@tryghost/admin-api');
       version: 'v4',
     });
 
+    console.log(directoryPath);
+    console.log(process.cwd() + '/liebling-custom.zip');
+
+    fs.readdir(directoryPath, function (err, files) {
+      //handling error
+      if (err) {
+        return console.log('Unable to scan directory: ' + err);
+      }
+      //listing all files using forEach
+      files.forEach((file) => {
+        // Do whatever you want to do with the file
+        if (file.endsWith('.zip')) {
+          console.warn(file);
+        } else {
+          console.log(file);
+        }
+      });
+    });
+
     // Deploy it to the configured site
-    await api.themes.upload({ file: process.cwd() + '/liebling-custom.zip' });
+    await api.themes.upload({ file: directoryPath });
     console.log('Theme successfully uploaded!');
   } catch (err) {
     console.error(err);
