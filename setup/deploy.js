1de99a528d982abb5789301e0330e3607e3a4726
Eloy Castelo <e.castelo.otero@gmail.com>
2022-03-03 18:42:54 +0100

    fix: try to list all files

--- setup/deploy.js
+++ setup/deploy.js
@@ -1,12 +1,33 @@
 require('dotenv').config();
 const GhostAdminApi = require('@tryghost/admin-api');
 
+const path = require('path');
+const fs = require('fs');
+//joining path of directory
+const directoryPath = path.join(__dirname);
+//passsing directoryPath and callback function
+
 (async function main() {
   try {
     const api = new GhostAdminApi({
       url: process.env.GHOST_ADMIN_API_URL,
       key: process.env.GHOST_ADMIN_API_KEY,
       version: 'v4',
+    });
+
+    console.log(__dirname);
+    console.log(process.cwd());
+
+    fs.readdir(directoryPath, function (err, files) {
+      //handling error
+      if (err) {
+        return console.log('Unable to scan directory: ' + err);
+      }
+      //listing all files using forEach
+      files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file);
+      });
     });
 
     // Deploy it to the configured site
