58bdbe1337cdb7a933b9b9e3f386a115cf75ebd3
Eloy Castelo <e.castelo.otero@gmail.com>
2022-03-04 12:46:33 +0100

    fix: make automatic deployment work v4

--- setup/deploy.js
+++ setup/deploy.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const GhostAdminApi = require('@tryghost/admin-api');
 
+const FormData = require('form-data');
 const path = require('path');
 const fs = require('fs');
 //joining path of directory
@@ -18,24 +19,30 @@ const directoryPath = path.join(process.cwd(), 'liebling-custom.zip');
     console.log(directoryPath);
     console.log(process.cwd() + '/liebling-custom.zip');
 
-    fs.readdir(process.cwd(), function (err, files) {
-      //handling error
-      if (err) {
-        return console.log('Unable to scan directory: ' + err);
-      }
-      //listing all files using forEach
-      files.forEach((file) => {
-        // Do whatever you want to do with the file
-        if (file.endsWith('.zip')) {
-          console.warn(file);
-        } else {
-          console.log(file);
-        }
-      });
-    });
+    if (fs.existsSync(directoryPath)) {
+      console.log('EXISTS!!');
+    }
+    const formData = new FormData();
+    formData.append('file', fs.createReadStream(directoryPath));
+
+    // fs.readdir(process.cwd(), function (err, files) {
+    //   //handling error
+    //   if (err) {
+    //     return console.log('Unable to scan directory: ' + err);
+    //   }
+    //   //listing all files using forEach
+    //   files.forEach((file) => {
+    //     // Do whatever you want to do with the file
+    //     if (file.endsWith('.zip')) {
+    //       console.warn(file);
+    //     } else {
+    //       console.log(file);
+    //     }
+    //   });
+    // });
 
     // Deploy it to the configured site
-    await api.themes.upload({ file: directoryPath });
+    await api.themes.upload(formData);
     console.log('Theme successfully uploaded!');
   } catch (err) {
     console.error(err);
