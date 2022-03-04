fbdbdd70416ea6c9f692c578e1378963d0208c4c
Eloy Castelo <e.castelo.otero@gmail.com>
2022-03-04 17:41:18 +0100

    chore: enhance gh actions steps

--- setup/deploy.js
+++ setup/deploy.js
@@ -1,13 +1,6 @@
 require('dotenv').config();
 const GhostAdminApi = require('@tryghost/admin-api');
 
-const FormData = require('form-data');
-const path = require('path');
-const fs = require('fs');
-//joining path of directory
-const directoryPath = path.join(process.cwd(), 'liebling-custom.zip');
-//passsing directoryPath and callback function
-
 (async function main() {
   try {
     const api = new GhostAdminApi({
@@ -16,33 +9,7 @@ const directoryPath = path.join(process.cwd(), 'liebling-custom.zip');
       version: 'v4',
     });
 
-    console.log(directoryPath);
-    console.log(process.cwd() + '/liebling-custom.zip');
-
-    if (fs.existsSync(directoryPath)) {
-      console.log('EXISTS!!');
-    }
-    const formData = new FormData();
-    formData.append('file', fs.createReadStream(directoryPath));
-
-    // fs.readdir(process.cwd(), function (err, files) {
-    //   //handling error
-    //   if (err) {
-    //     return console.log('Unable to scan directory: ' + err);
-    //   }
-    //   //listing all files using forEach
-    //   files.forEach((file) => {
-    //     // Do whatever you want to do with the file
-    //     if (file.endsWith('.zip')) {
-    //       console.warn(file);
-    //     } else {
-    //       console.log(file);
-    //     }
-    //   });
-    // });
-
-    // Deploy it to the configured site
-    await api.themes.upload(formData);
+    await api.themes.upload({ file: '../liebling-custom.zip' });
     console.log('Theme successfully uploaded!');
   } catch (err) {
     console.error(err);
